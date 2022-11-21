a47f0c70d16cb8529c73e287c24fa861da8fd731
ysaianusha <70245925+ysaianusha@users.noreply.github.com>
2022-11-21 16:29:37 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+'''Given a matrix of 0's and 1's of size 256x256, write a program to print minimum sized sub-rectangle which includes all 0's of the matrix. 
+Function needs to return the 4 corners of the minimum sized sub-rectangle.
+
+Input:
+1 0 0 1 1
+1 0 0 0 1
+1 0 0 1 1
+1 1 1 1 1
+Output
+(0,1), (0, 3), (2, 1), (2, 3)'''
+
